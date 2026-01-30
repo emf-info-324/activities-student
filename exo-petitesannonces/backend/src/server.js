459ccfe3393d1c6faeeb559eaad1745e767bb13d
Pires Donose Andrei <143510929+piresdonose-andrei-emf@users.noreply.github.com>
2026-01-30 09:39:55 +0100

    update

--- exo-petitesannonces/backend/src/server.js
+++ exo-petitesannonces/backend/src/server.js
@@ -1,25 +1,27 @@
 /**
  * Endpoint principal de l'application.
- * 
+ *
  * @author Les enseignants EMF du module 324
  * @version 1.0
  */
-require("dotenv").config();
-const config = require('./config/config');
-const cors = require('cors');
-const bodyParser = require('body-parser');
 
+const config = require("./config/config");
+const port = config.port || 3000;
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-const annoncesRoutes = require('./routes/annonceRoutes');
+const annoncesRoutes = require("./routes/annonceRoutes");
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/annonces', annoncesRoutes);
+app.use("/annonces", annoncesRoutes);
 
-app.listen(config.port, () => {
-  console.log(`Server is running on port ${config.port}`);
+
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running on port ${port}`);
 });
